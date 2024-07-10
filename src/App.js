f912114b7e673c624dba8518f5b57ac8cc6fa57b
EmptyEllan <im.alanfree@gmail.com>
2024-07-11 06:15:25 +0800

    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './Home/Home';
-// import About from './About/About';
-// import Contact from './Contact/Contact';
-// import MinecraftTools from './MinecraftTools/MinecraftTools';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import DragonMasterDetails from './DragonMasterDetails/DragonMasterDetails';
-// import NavBar from './NavBar/NavBar';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
@@ -16,13 +11,8 @@ function App() {
   return (
     <AppContainer>
       <Router>
-        {/* <NavBar /> */}
         <Routes>
           <Route path="/" element={<DragonMasterDetails />} />
-          {/* <Route path="/about" element={<About />} /> */}
-          {/* <Route path="/contact" element={<Contact />} /> */}
-          {/* <Route path="/minecrafttools" element={<MinecraftTools />} /> */}
-          {/* <Route path="/dragon_master_details" element={<DragonMasterDetails />} /> */}
         </Routes>
       </Router>
     </AppContainer>
